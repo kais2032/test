74396dc7ebd94baa17a1f8ddade06af8a37e4227
kais2032 <113527452+kais2032@users.noreply.github.com>
2022-09-17 15:37:27 +0200

    new commit test

--- src/testDecomposition/main.java
+++ src/testDecomposition/main.java
@@ -5,6 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		Personne p=new Personne("kais",2);
 		Etudiant e=new Etudiant("kais",2);
+		Etudiant e=new Etudiant("kais",2);
 Etudiant e=new Etudiant("kais",2);
 	}
 }
